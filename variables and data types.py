0e64ffe1d13258236176b10023997dc65b5e8ac6
Abdullah Nasir Chowdhury <147189502+Abdullah-Nasir-Chowdhury@users.noreply.github.com>
2023-10-18 12:12:13 +0600

    Add files via upload

---
+++ variables and data types.py
@@ -0,0 +1,21 @@
+# data types:
+# 4 main data types: int, float, string, bool
+
+
+# initialize variables:
+# rules: no space in names, no number to start, no predefined variable names
+# no * or special symbols in the variables
+int_var = 3
+float_var = 3.45
+string_var = "godlike kills"
+bool_var1 = True
+bool_var2 = False
+
+
+# print variables:
+print(int_var)
+print(float_var)
+print(string_var)
+print(bool_var1)
+print(bool_var2)
+
