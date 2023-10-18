02515f8504aa77695bc02d89787ea6b1a14a9554
Abdullah Nasir Chowdhury <147189502+Abdullah-Nasir-Chowdhury@users.noreply.github.com>
2023-10-18 12:40:24 +0600

    Add files via upload

---
+++ Take input.py
@@ -0,0 +1,6 @@
+print('Hello, what is your name? ')
+name = input()
+print(f"Hello {name}, How are you doing! ")
+How_are_you = input()
+print(f"You are {How_are_you}")
+
---
+++ type conversion.py
@@ -0,0 +1,16 @@
+integer = 5
+string = 'Abdullah'
+Float = 5.5
+
+# check types:
+type(integer)
+type(string)
+type(Float)
+
+# convert types:
+float_to_int=int(Float)
+# int(string) you will get error
+int_to_str=str(integer)
+float_to_str=str(Float)
+int_to_float=float(integer)
+# float(string) you will get error
