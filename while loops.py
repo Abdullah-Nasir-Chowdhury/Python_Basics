b4646e121b5d0467cdd57b18e2259605c3151434
Abdullah Nasir Chowdhury <147189502+Abdullah-Nasir-Chowdhury@users.noreply.github.com>
2023-10-19 06:22:16 +0600

    Add files via upload

---
+++ while loops.py
@@ -0,0 +1,9 @@
+loop = True
+
+while loop:
+    name = input('Insert something: ')
+    if name == 'stop':
+        loop = False
+
+#  while loop is used when you don't know the range
+# for loop is when you have the range
