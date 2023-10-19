bb57c3e46fd9437e10fcb892bcae74ea44fe3198
Abdullah Nasir Chowdhury <147189502+Abdullah-Nasir-Chowdhury@users.noreply.github.com>
2023-10-19 06:33:16 +0600

    Add files via upload

---
+++ Lists and Tuples.py
@@ -0,0 +1,21 @@
+# initiate list:
+fruits = ['apple', 'pear', 3]
+print(fruits)
+# access individual items, indices:
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+# add items to the list:
+fruits.append('mango')
+print(fruits)
+# add another list to list:
+fruits.append(['jackfruit','litchii'])
+print(fruits)
+# manipulate data in list:
+fruits[2] = 'banana'
+print(fruits)
+
+# initiate tuple: used for co-ordinates, colors, rectangles,
+# can't be changed!
+price = (1,2,3,4,5)
+
